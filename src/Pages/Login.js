9efcebc4a3018f7469efe5deda2a2c61bf24e172
Himanshu Sharma <himanshug1310@gmail.com>
2025-06-06 15:10:15 +0530

    fix: Adjust layout and typography in Login component for improved aesthetics

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -101,10 +101,10 @@ const Login = (props) => {
         <div className="overflow-hidden h-[100%] w-[100%]">
             <div className="flex items-center justify-center h-[100%] z-10 relative w-[100%]" >
                 <img src={mapImage} alt="Map" className="w-full h-[100%] object-cover opacity-20 absolute" />
-                <div className="flex flex-col items-center p-6 gap-3 bg-white bg-opacity-30 rounded-lg shadow-lg w-[32%] z-10">
+                <div className="flex flex-col items-center p-6 gap-3 bg-white bg-opacity-30 rounded-lg shadow-lg w-[35%] min-w-[300px] z-10">
                     <div className="flex flex-col items-center gap-2">
-                        <h1 className="font-bold text-4xl">Welcome Back !</h1>
-                        <p className="text-gray-700 text-md">Hey, glad to see you back here</p>
+                        <h1 className="font-bold text-2xl sm:text-2xl md:text-3xl lg:text-4xl">Welcome Back !</h1>
+                        <p className="text-gray-700 text-sm sm:text-md md:text-md lg:text-md">Hey, glad to see you back here</p>
                     </div>
                     <form className="flex flex-col gap-4 w-[100%] mt-0" onSubmit={submitHandler}>
                         <input type="email" placeholder="Email" value={loginData.email} id="email" name="email"
